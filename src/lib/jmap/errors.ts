b96962d7256dec57e13d24e3a18c2a783104e66e
thomascanning <tom@thomascanning.co.uk>
2025-11-17 16:55:07 +0000

    defining urn types

---
+++ src/lib/jmap/errors.ts
@@ -0,0 +1,8 @@
+export type unknownCapabilityUrn = "urn:ietf:params:jmap:error:unknownCapability"
+
+export type notJsonUrn = "urn:ietf:params:jmap:error:notJSON"
+
+export type notRequestUrn = "urn:ietf:params:jmap:error:notRequest"
+
+//Must include limit property (name of limit) in problem details response
+export type limitUrn = "urn:ietf:params:jmap:error:limit"
